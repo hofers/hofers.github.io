ef24d23e1b7982b630817e682326ee28a7a6bb38
Sean Hofer <sean@feedingchildreneverywhere.com>
2017-10-03 11:35:59 -0400

    updated resume & linkedin link

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,24 +1,18 @@
 //Sean Hofer 2015
 //there's not much in here, animations are all handled via CSS
 
-$(function() {
+$(function () {
 
   //keeps track of which header tab is active
-  $('a.headItem').on('click', function() {
+  $('a.headItem').on('click', function () {
     var activeDiv = $("a.activeHeadItem").text();
     $("#" + activeDiv).removeClass('activeSubsection');
-    if(activeDiv == "home"){
-      d3.select("svg").attr("class", " ");
-    }
 
     $("a.activeHeadItem").removeClass('activeHeadItem');
     $(this).addClass('activeHeadItem');
 
     activeDiv = $(this).text();
     $("#" + activeDiv).addClass('activeSubsection');
-    if(activeDiv == "home"){
-      d3.select("svg").attr("class", "activeSubsection");
-    }
   });
 
 });
