1a53a77b093d0df51f94fbde0bcdb7c242bef043
Sean Hofer <sean@feedingchildreneverywhere.com>
2018-09-20 13:16:49 -0400

    update #1 colors and copy - no portfolio section yet

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,10 @@
-//Sean Hofer 2015
-//there's not much in here, animations are all handled via CSS
+// Sean Hofer 2015
+// not much in here, animations are all handled via CSS
+// I know I don't need jQuery but it's here because I like it okay?
 
 $(function () {
 
-  //keeps track of which header tab is active
+  // keeps track of which header tab is active
   $('a.headItem').on('click', function () {
     var activeDiv = $("a.activeHeadItem").text();
     $("#" + activeDiv).removeClass('activeSubsection');
