66ce9ab0fda85d32046819f1009561e64a050c5b
Sean Hofer <Sean@Laurens-MacBook-Pro-2.local>
2019-08-25 20:56:19 -0700

    lint

--- assets/js/ga.js
+++ assets/js/ga.js
@@ -1,29 +1,29 @@
-window.dataLayer = window.dataLayer || [];
+window.dataLayer = window.dataLayer || []
 
-function gtag() {
-  dataLayer.push(arguments);
+function gtag () {
+  dataLayer.push(arguments)
 }
 
-gtag('js', new Date());
+gtag('js', new Date())
 
 gtag('config', 'UA-143817336-1', {
-  'link_attribution': true
-});
+  link_attribution: true
+})
 
 var getOutboundLink = function (url) {
   gtag('event', 'click', {
-    'event_category': 'outbound',
-    'event_label': url,
-    'transport_type': 'beacon',
-    'event_callback': function () {
-      document.location = url;
+    event_category: 'outbound',
+    event_label: url,
+    transport_type: 'beacon',
+    event_callback: function () {
+      document.location = url
     }
-  });
+  })
 }
 
 // Add outbound link click event to all outbound links
-document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventListener('DOMContentLoaded', function (event) {
   document.querySelectorAll("a[target='_blank']").forEach((e) => {
-    e.setAttribute("onclick", "getOutboundLink('" + e.getAttribute("href") + "');")
-  });
-});
+    e.setAttribute('onclick', "getOutboundLink('" + e.getAttribute('href') + "');")
+  })
+})
