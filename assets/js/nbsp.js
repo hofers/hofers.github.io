3666b184220a7ee364c207faac05273d96f44b8d
Sean Hofer <Sean@MacBook-Pro.local>
2020-06-06 16:54:22 -0700

    replace nbsp js implementation with Jekyll

--- assets/js/nbsp.js
+++
@@ -1,8 +0,0 @@
-// Replaces last space in each p tag with nbsp;
-window.onload = () => {
-  var paragraphs = document.getElementsByTagName("p");
-  for (let p of paragraphs) {
-    let html = p.innerHTML;
-    p.innerHTML = html.substring(0,html.lastIndexOf(" ")) + "&nbsp;" + html.substring(html.lastIndexOf(" ")+1);
-  }
-};
